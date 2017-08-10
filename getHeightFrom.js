631fd979c51ab630593a2c5278e275625f845e9d
Tobias Kohlhepp <tk@root-shell.de>
2017-08-10 20:59:15 +0200

    Description text

--- getHeightFrom.js
+++ getHeightFrom.js
@@ -1,39 +1,53 @@
 
-// Holt die höhe des Elementes welches die ID besitzt welche mit data-target-id übergeben wurde
+/* *****************************************************************************
+Get the height from an element with the target-id
+
+Example:
+
+$(".getHeightFrom").getHeightFrom();
+
+<div class="getHeightFrom" data-target-id="target-box"></div>
+<div id="target-box"></div>
+
+or
+
+$(".getHeightFromMax991").getHeightFrom({maxViewport:'991'});
+
+***************************************************************************** */
 
 (function($){
-	
+
 	$.fn.getHeightFrom=function( options ){
-		
+
 		var settings = $.extend({
             // defaults.
             maxViewport: "767",
         }, options );
-		
+
 		var $cells = this;
-		
+
 		if ( jQuery(window).width() > settings.maxViewport ) {
 			resizeTheCell($cells);
-			
+
 			$(window).load(function () {
 				resizeTheCell($cells);
 			});
 		}
-		
+
 		$(window).resize(function() {
 			if ( jQuery(window).width() > settings.maxViewport ) {
 				resizeTheCell($cells);
 			}
 		});
 	}
-	
+
 	var resizeTheCell = function( cells ) {
-		
+
 		$(cells).each(function(){
-			
+
 			var $target_id = "#" + $(this).attr('data-target-id');
 			$target_height = $($target_id).height();
-			
+
 			// Fix, wenn gleichzeitig text_center.js verwendet wird
 			$(this).find(".content").css({
 				'padding-top': 0
@@ -42,10 +56,10 @@
 			$(this).css({
 				'min-height': $target_height
 			});
-			
+
 		});
-		
+
 
 	}
-	
+
 })(jQuery);
